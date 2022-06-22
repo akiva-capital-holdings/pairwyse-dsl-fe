ce1ff091c20e5db9c8ad7040658fa95c3559edb6
yuliia gorgodze <taragorgodze@gmail.com>
2022-06-22 12:37:26 +0300

    fix build

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -32,7 +32,7 @@ const HomePage = () => {
   };
 
   const steps = {
-    stepOne: <AgreementRequest  onChangeStep={onChangeStep}/>,
+    stepOne: <AgreementRequest />,
     stepTwo: <DefinitionRequest />,
     stepThree: <UpdateRequest />,
   };
