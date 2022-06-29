ba0eec0580921a4f6fd5fa1b6f316f81ec24233b
Misha Kushka <kushka.misha@gmail.com>
2022-06-29 10:14:21 +0200

    Fix definition request screen

--- src/components/home/steps/definitionRequest.tsx
+++ src/components/home/steps/definitionRequest.tsx
@@ -9,55 +9,60 @@ import { useSelector } from 'react-redux';
 import { selectUtils } from 'redux/utilsReducer';
 import { createInstance, hex4Bytes } from 'utils/helpers';
 import { selectSession } from 'redux/sessionReducer';
-import getRule from '../../../utils/validate'
+import getRule from '../../../utils/validate';
 import { ReactComponent as Delete } from '../../../images/delete.svg';
 
-const {Item} = Form;
+const { Item } = Form;
 // import {selectUtils} from '../../../redux/utilsReducer'
 
-const mock = [
-  { title: 'specification', value: '0x25eca5c18cf82asdfs91bc168de7', id: 1 },
-];
+const mock = [{ title: 'Specification (address)', value: '', id: 1 }];
 const DefinitionRequest = () => {
   const { address: userWallet } = useSelector(selectSession);
   const [definition, setDefinition] = useState('');
   const { provider } = useSelector(selectUtils);
-  const [specification, setspecification] = useState(mock);
-  const [agreement, setAgreement] = useState('')
+  const [specifications, setspecification] = useState(mock);
+  const [agreement, setAgreement] = useState('');
   const navigate = useNavigate();
 
-  //  const onSubmit = async () => {
-  //     const membershipInstance: any = await definitionInstance(
-  //    `${process.env.REACT_APP_AGREEMENT_FACTORY}`,
-  //     provider,
-  //    );
-  //    const get = await membershipInstance.methods  // @Misha call method
-  //   }
-
   const defineVariable = async () => {
-    const a = await createInstance('Agreement', agreement, provider);
+    // eslint-disable-next-line
+    const _agreementAddr = agreement;
+    // eslint-disable-next-line
+    const _definition = definition;
+    // eslint-disable-next-line
+    const _specification = specifications[0].value;
+
+    console.log({
+      _agreementAddr,
+      _definition,
+      _specification,
+    });
+
+    const a = await createInstance('Agreement', _agreementAddr, provider);
     const txsAddr = await a.methods.txs().call();
     console.log({ txsAddr });
     const txs = await createInstance('ConditionalTxs', txsAddr, provider);
-    const tx = await txs.methods.setStorageAddress(hex4Bytes(definition), specification).send({ from: userWallet });
+    const tx = await txs.methods
+      .setStorageAddress(hex4Bytes(_definition), _specification)
+      .send({ from: userWallet });
     console.log({ tx });
     // Check that the variable was set
-    const value = await txs.methods.getStorageAddress(hex4Bytes(definition)).call();
+    const value = await txs.methods.getStorageAddress(hex4Bytes(_definition)).call();
     console.log({ value });
   };
- 
+
   return (
     <div className="definitionRequest">
       <div className="title">DefinitionRequest</div>
       <Form name="agreementRequestForm" autoComplete="off" onFinish={defineVariable}>
-      <div style={{ marginTop: '24px' }} className="text">
+        <div style={{ marginTop: '24px' }} className="text">
           Requestor
         </div>
         <div className="value">{userWallet}</div>
         <div style={{ marginTop: '24px' }} className="text">
           Agreement
-      </div>
-      <Item name='agreement' validateTrigger="onBlur" rules={getRule('agreement', 'agreement')}>
+        </div>
+        <Item name="agreement" validateTrigger="onBlur" rules={getRule('agreement', 'agreement')}>
           <Input
             className="lander"
             value={agreement}
@@ -65,62 +70,72 @@ const DefinitionRequest = () => {
               return setAgreement(e?.target?.value);
             }}
           />
-      </Item>
+        </Item>
         <div style={{ marginTop: '24px' }} className="text">
           Definition
         </div>
-        <Item name='borrower' validateTrigger="onBlur" rules={getRule('definition', 'definition')}>
-            <Input
-              placeholder='Borrower'
-              className="lander"
-              value={definition}
-              onChange={(e) => {
-                return setDefinition(e?.target?.value);
-              }}
-            />
+        <Item name="borrower" validateTrigger="onBlur" rules={getRule('definition', 'definition')}>
+          <Input
+            placeholder="Borrower"
+            className="lander"
+            value={definition}
+            onChange={(e) => {
+              return setDefinition(e?.target?.value);
+            }}
+          />
         </Item>
         <div className="specification">
-        {specification.map((el) => {
-        return (
-          <div className="specificationInput" key={el.id}>
-            <div style={{ marginTop: '24px' }} className="text">
-              {el.title}{' '}
-            </div>
-            <Item name={`specification${el.id}`} validateTrigger="onBlur" rules={getRule('specification', 'specification', el.value)}>
-              <Input
-                onChange={e => setspecification(specification?.map(c => c?.id === el?.id ?  {...c, value: e?.target.value} : {...c}))}
-                className="lander"
-              />
-            </Item>
-            <Button
-              htmlType='button'
-              onClick={() => setspecification(specification.filter((s) => s.id !== el.id))}
-              className="del"
-            >
-             {specification?.length > 1 &&  <Delete />}
-            </Button>
-          </div>
-        );
-      })}
+          {specifications.map((el) => {
+            return (
+              <div className="specificationInput" key={el.id}>
+                <div style={{ marginTop: '24px' }} className="text">
+                  {el.title}{' '}
+                </div>
+                <Item
+                  name={`specification${el.id}`}
+                  validateTrigger="onBlur"
+                  rules={getRule('specification', 'specification', el.value)}
+                >
+                  <Input
+                    onChange={(e) =>
+                      setspecification(
+                        specifications?.map((c) =>
+                          c?.id === el?.id ? { ...c, value: e?.target.value } : { ...c }
+                        )
+                      )
+                    }
+                    className="lander"
+                  />
+                </Item>
+                <Button
+                  htmlType="button"
+                  onClick={() => setspecification(specifications.filter((s) => s.id !== el.id))}
+                  className="del"
+                >
+                  {specifications?.length > 1 && <Delete />}
+                </Button>
+              </div>
+            );
+          })}
           <Button
-           htmlType='button'
-           className="add"
-           onClick={() =>
-            setspecification([
-               ...specification,
-               { title: `specification ${specification?.length}`, value: '', id: specification?.length + 1 },
-             ])
-           }
-         >
-           Add Specification
-         </Button>
+            htmlType="button"
+            className="add"
+            onClick={() =>
+              setspecification([
+                ...specifications,
+                {
+                  title: `Specification ${specifications?.length}`,
+                  value: '',
+                  id: specifications?.length + 1,
+                },
+              ])
+            }
+          >
+            Add Specification
+          </Button>
         </div>
         <div className="btnsContainer">
-          <Button
-            style={{ height: '48px' }}
-            htmlType="submit"
-            className="btn"
-          >
+          <Button style={{ height: '48px' }} htmlType="submit" className="btn">
             Request Approval
           </Button>
           <Button
@@ -133,7 +148,7 @@ const DefinitionRequest = () => {
             Cancel
           </Button>
         </div>
-      </Form>   
+      </Form>
     </div>
   );
 };
--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,86 +1,90 @@
-import {ethers} from 'ethers'
+import { ethers } from 'ethers';
 
 export default function getRule(label: string, name: string, v?) {
   const defaultRule = {
     required: true,
     message: `This field ${label.toLowerCase()} is required`,
   };
-  const validateMinMax = (min: number, max: number) => {return {
-    validator: (_: any, value: string) => {
-      if (value?.length < min) {
-        return Promise.reject(
-          new Error(`Field ${name} must be more than ${min} symbols`),
-        );
-      }
-      if (value?.length > max) {
-        return Promise.reject(
-          new Error(`Field ${name} must be more than  ${max} symbols`),
-        );
-      }
-      return Promise.resolve();
-    },
-  }}
+  const validateMinMax = (min: number, max: number) => {
+    return {
+      validator: (_: any, value: string) => {
+        console.log({ valLen: value?.length });
+        if (value?.length < min) {
+          return Promise.reject(new Error(`Field ${name} must be less than ${min} symbols`));
+        }
+        if (value?.length > max) {
+          return Promise.reject(new Error(`Field ${name} must be more than  ${max} symbols`));
+        }
+        return Promise.resolve();
+      },
+    };
+  };
 
+  const simbols = /^[A-Za-z0-9_.]+$/;
+  const ruAlfabet = /^[а-яА-ЯёЁ]+$/;
 
-const simbols = /^[A-Za-z0-9_.]+$/
-const ruAlfabet = /^[а-яА-ЯёЁ]+$/
+  const validateField = () => {
+    return {
+      validator: (_: any, value: string) => {
+        if (!value) return Promise.resolve();
+        if (simbols.test(value)) return Promise.resolve();
+        return Promise.reject(new Error(`This field ${label.toLowerCase()} is required`));
+      },
+    };
+  };
 
-const validateField = () => {return {
-  validator: (_: any, value: string) => {
-    if (!value) return Promise.resolve();
-    if (simbols.test(value)) return Promise.resolve();
-    return Promise.reject(new Error(`This field ${label.toLowerCase()} is required`),);
-  },
-}};
+  const validateFieldCondition = () => {
+    return {
+      validator: (_: any, value: string) => {
+        if (!value) return Promise.resolve();
+        if (!ruAlfabet.test(value)) return Promise.resolve();
+        return Promise.reject(new Error('Invalid format'));
+      },
+    };
+  };
 
-const validateFieldCondition = () => {return {
-  validator: (_: any, value: string) => {
-    if (!value) return Promise.resolve();
-    if (!ruAlfabet.test(value)) return Promise.resolve();
-      return Promise.reject(new Error('Invalid format'),)
-  },
-}};
+  const validationAgreementModel = () => {
+    return {
+      validator: () => {
+        if ((v && v?.length === 0) || v?.length === 1) {
+          return Promise.reject(new Error(`This field ${label.toLowerCase()} is required`));
+        }
 
-  const validationAgreementModel = () => {return {
-    validator: () => {
-      if (v && v?.length === 0 || v?.length ===  1) {
-        return Promise.reject(
-          new Error(`This field ${label.toLowerCase()} is required`),
-        );
-      }
+        return Promise.resolve();
+      },
+    };
+  };
 
-      return Promise.resolve();
-    },
-  }}
+  const validateAddressEth = () => {
+    return {
+      validator: (_: any, value: string) => {
+        if (!value) return Promise.resolve();
+        if (ethers.utils.isAddress(value)) return Promise.resolve();
+        return Promise.reject(new Error('Invalid format'));
+      },
+    };
+  };
 
-  const validateAddressEth = () => {return {
-    validator: (_: any, value: string) => {
-      if (!value) return Promise.resolve();
-      if (ethers.utils.isAddress(value)) return Promise.resolve();
-      return Promise.reject(new Error('Invalid format'),);
-    },
-   }
-  }
-
-  const validateAddress = () => {return {
-    validator: (_: any, value: string) => {
-      if (!value) return Promise.resolve();
-      if (value !== '0x0000000000000000000000000000000000000000') return Promise.resolve();
-      return Promise.reject(new Error('Invalid address'),);
-    },
-   }
-  }
+  const validateAddress = () => {
+    return {
+      validator: (_: any, value: string) => {
+        if (!value) return Promise.resolve();
+        if (value !== '0x0000000000000000000000000000000000000000') return Promise.resolve();
+        return Promise.reject(new Error('Invalid address'));
+      },
+    };
+  };
   switch (name) {
     case 'requestorLabel':
       return [defaultRule, validateMinMax(0, 20)];
-    case 'agreement': 
-      return [defaultRule, validateAddressEth(), validateAddress()]
+    case 'agreement':
+      return [defaultRule, validateAddressEth(), validateAddress()];
     case 'agreement model':
       return [validationAgreementModel()];
     case 'definition':
       return [defaultRule, validateField(), validateMinMax(0, 20)];
     case 'specification':
-      return [defaultRule, validateField(), validateMinMax(0, 20)];
+      return [defaultRule, validateField(), validateMinMax(0, 42)];
     case 'condition':
       return [defaultRule, validateFieldCondition()];
     case 'signatories':
